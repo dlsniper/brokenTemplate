282c7bc78a2ac3b541583040b9353dd2fb71b7e0
Florin Patan <florinpatan@gmail.com>
2019-06-24 10:22:00 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module brokenTemplate
+
+go 1.13
---
+++ templ/file.go
@@ -0,0 +1,12 @@
+package mypage
+
+type PageData struct {
+	Field1 int
+	Field2 string
+	Content
+}
+
+type Content struct {
+	CField1 int
+	CField2 string
+}
